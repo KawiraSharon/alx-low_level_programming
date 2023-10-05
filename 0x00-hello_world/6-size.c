c54ba90c2e8c233feb42ed30eff7f2a2ec75d433
KawiraSharon <sharonkawira389@gmail.com>
2023-10-05 12:42:34 -0700

    Data Type values

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,13 +13,12 @@ long w;
 float m;
 long long int e;
 
-
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(z));
-printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(y));
-printf("Size of double: %lu byte(s)\n", (unsigned long)sizeof(p));
-printf("Size of long: %lu byte(s)\n", (unsigned long)sizeof(w));
-printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(m));
-printf("Size of long long: %lu byte(s)\n", (unsigned long)sizeof(e));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(z));
+	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(y));
+	printf("Size of double: %lu byte(s)\n", (unsigned long)sizeof(p));
+	printf("Size of long: %lu byte(s)\n", (unsigned long)sizeof(w));
+	printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(m));
+	printf("Size of long long: %lu byte(s)\n", (unsigned long)sizeof(e));
 return (0);
 
 }
