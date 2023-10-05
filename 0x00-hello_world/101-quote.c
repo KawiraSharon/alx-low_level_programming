9e7507b3fc518082d7eeadc3e184830570f33ffd
KawiraSharon <sharonkawira389@gmail.com>
2023-10-05 12:32:15 -0700

    Write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <unistd.h>
 /**
- * main - Printing exactly given line with new line to the standard error
- * Return: 1 as an indication of code success
+ * main - program to print line, then new line to the standard error
+ * Return: Always 0 to indicate code success
  */
-int main(void)
 
+int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 59");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
+
