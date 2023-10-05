c8ade646b6d49c1cf1262ecbf7df1c9699f95cce
KawiraSharon <sharonkawira389@gmail.com>
2023-10-05 10:10:52 -0700

    Betty check

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Program prints line with puts as the function
+ * Description: 'Betty'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
