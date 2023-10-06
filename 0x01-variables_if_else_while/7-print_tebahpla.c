ac92e0b832a52c53e1367671e9bd28576b2fb369
KawiraSharon <sharonkawira389@gmail.com>
2023-10-06 16:20:21 -0700

    Putchar Function

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - The Putchar function and code in main
+ *
+ * Return: Always 0 to indicate project success
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 0 ; i < 10 ; i++)
+	putchar(i + '0');
+putchar('\n');
+return (0);
+
+}
