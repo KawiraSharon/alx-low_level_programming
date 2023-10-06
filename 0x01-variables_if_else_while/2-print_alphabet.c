095aa1b45cc3ec09eb0966e8c5f0554ace186da3
KawiraSharon <sharonkawira389@gmail.com>
2023-10-06 12:39:35 -0700

    Insomniaic

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Program to use putchar function
+ *
+ * Return: Always 0 to indicate code success
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'b' ; a <= 'z' ; a++)
+		putchar(a);
+		putchar('\n');
+	return (0);
+
+}
