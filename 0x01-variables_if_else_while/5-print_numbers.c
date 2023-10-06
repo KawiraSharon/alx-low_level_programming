0fa321b6e6da27a2807c993d8783e63b66ccb758
KawiraSharon <sharonkawira389@gmail.com>
2023-10-06 15:51:19 -0700

    Code indent

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -Function of the main program encapsulated here
+ *
+ * Return: Always 0 to indicate code success
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 0 ; i < 10 ; i++)
+	printf("%d", i);
+printf("\n");
+return (0);
+
+}
