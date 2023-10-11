47e339c9db7ec01b9cf9031239624acce95dbdfb
KawiraSharon <sharonkawira389@gmail.com>
2023-10-11 11:35:54 -0700

    Alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,7 @@
  * Return: Always return (0)
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 char c;
 int i = 0;
@@ -19,4 +19,5 @@ _putchar (c);
 _putchar ('\n');
 i++;
 }
+
 }
