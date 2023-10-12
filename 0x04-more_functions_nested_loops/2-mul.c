cae6bf1d1a1dac14599aa0fcde1a386b41e416bd
KawiraSharon <sharonkawira389@gmail.com>
2023-10-12 15:01:08 -0700

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Program to compute parameter multiplication
+ * @x: Parameter 1 defined
+ * @y: Parameter 2 defined
+ * Return: return 0 for code success
+ */
+
+int mul(int x, int y)
+{
+return (x * y);
+
+}
