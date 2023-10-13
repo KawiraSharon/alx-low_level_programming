a94c3a7e502cbb11efbae8e214dc2a037b33225a
KawiraSharon <sharonkawira389@gmail.com>
2023-10-13 10:40:36 -0700

    Debug 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* main -function checks if integers are positive or converse
+* Return: 0 on success
+*/
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+
+}
+
---
+++ 0x03-debugging/main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main -tests functions to print if integers are positive or converse
+* Return: 0 on code success
+*/
+
+int main(void)
+{
+int i;
+
+i = 98;
+positive_or_negative(i);
+
+return (0);
+
+}
