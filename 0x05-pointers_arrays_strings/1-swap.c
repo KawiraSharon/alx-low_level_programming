959c72c85530c862390e1f34b767e1d7f780e543
KawiraSharon <sharonkawira389@gmail.com>
2023-10-16 07:20:46 -0700

    Horses

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - program to change two integers
+ * @a: parameter one to swap in integer
+ * @b: parameter two to swap in integer
+ */
+
+void swap_int(int *a, int *b)
+
+{
+int m;
+
+m = *a;
+*a = *b;
+*b = m;
+
+}
+
