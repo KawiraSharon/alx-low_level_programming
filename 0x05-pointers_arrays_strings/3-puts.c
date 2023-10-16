38d4bb3c6c701726d680a13c2906d5f0a38d7dea
KawiraSharon <sharonkawira389@gmail.com>
2023-10-16 07:38:08 -0700

    Fear lack of comps

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - program to output string then new line, to stdout
+ * @str: as defined
+ */
+
+void _puts(char *str)
+
+{
+while (*str != '\0')
+
+{
+_putchar(*str++);
+}
+_putchar('\n');
+
+}
+
