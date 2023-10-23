ffc3f4485ee4b6bfb5e373e71c7c606f46d1ba39
KawiraSharon <sharonkawira389@gmail.com>
2023-10-23 14:01:12 -0700

    Doublefun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - Entry point of program
+ * @s: input for parameter 1
+ * @to: input for parameter 2
+ *
+ * Return: 0 on code success
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
