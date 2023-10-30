f05d3792a690234bb427292ef742d112dac58c07
KawiraSharon <sharonkawira389@gmail.com>
2023-10-30 15:15:27 +0300

    fLoat like a butterfly

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * create_array - function name
+ * @size - denotes size of elements in array
+ * @c - denotes the value of the elements array will consist
+ * 
+ * Return - the pointer
+ */
 char *create_array(unsigned int size, char c)
 {
     char *ptArray;
