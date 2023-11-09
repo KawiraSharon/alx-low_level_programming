131beac41d8b8c97c4cd72359b57954b9bc904eb
KawiraSharon <sharonkawira389@gmail.com>
2023-11-09 17:38:15 +0300

    PrintString

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - function to print string followed by \n.
+ * @separator: string to print btwn strngs
+ * @n: no of arguments in strings user enters
+ * @... user elements user enters
+ * Author: KawiraSharon
+ * Return: nth as return data type of func is void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int x;
+	char *strng;
+	va_list stringNew;
+
+	va_start(stringNew, n);
+
+	for (x = 0; x < n; x++)
+	{
+		printf("%s", va_arg(stringNew, char));
+
+		if (strng != NULL)
+			printf("%s", strng);
+
+		else
+			printf("%s", nil);
+
+		if (x != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(stringNew);
+}
