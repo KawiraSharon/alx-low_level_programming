8633bc95eb6f575197ad98d40bff924f10fe59aa
KawiraSharon <sharonkawira389@gmail.com>
2023-11-09 20:22:38 +0300

    printAll

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,52 +1,51 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
- * print_all - prints anything based on format specified
- * @format: list of types of arguments passed to function
- *
- * Return: void data type so nothing is returned
+ * print_all - print everything entered by user
+ * @format: list char, float, int, string as specified for user to enter
  */
 void print_all(const char * const format, ...)
 {
 	va_list outputAll;
-	unsigned int x = 0;
+	unsigned int x = 0, y, z = 0;
 	char *str;
-	const char *separator = "";
 	const char t_arg[] = "cifs";
 
 	va_start(outputAll, format);
 	while (format && format[x])
 	{
-		unsigned int y = 0;
-
+		y = 0;
 		while (t_arg[y])
 		{
-			if (format[x] == t_arg[y])
+			if (format[x] == t_arg[y] && z)
 			{
-				printf("%s", separator);
-				if (format[x] == 'c')
-					printf("%c", va_arg(outputAll, int));
-				else if (format[x] == 'i')
-					printf("%d", va_arg(outputAll, int));
-				else if (format[x] == 'f')
-					printf("%f", va_arg(outputAll, double));
-				else if (format[x] == 's')
-				{
-					str = va_arg(outputAll, char *);
-					if (!str)
-						printf("(nil)");
-					else
-						printf("%s", str);
-				}
-				separator = ", ";
+				printf(", ");
+				break;
+			} y++;
+		}
+		switch (format[x])
+		{
+		case 'c':
+			printf("%c", va_arg(outputAll, int)), z = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(outputAll, int)), z = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(outputAll, double)), z = 1;
+			break;
+		case 's':
+			str = va_arg(outputAll, char *), z = 1;
+			if (!str)
+			{
+				printf("(nil)");
 				break;
 			}
-			y++;
-		}
-		x++;
+			printf("%s", str);
+			break;
+		} x++;
 	}
-	printf("\n");
-	va_end(outputAll);
+	printf("\n"), va_end(outputAll);
 }
