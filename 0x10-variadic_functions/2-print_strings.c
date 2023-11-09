d9fefbe09025c2b371f74388df4196e7ed9246fe
KawiraSharon <sharonkawira389@gmail.com>
2023-11-09 21:29:12 +0300

    printString

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -32,5 +32,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", separator);
 	}
 
-	printf("\n"), va_end(stringNew);
+	printf("\n");
+	
+	va_end(stringNew);
 }
