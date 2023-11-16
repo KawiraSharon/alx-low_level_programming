90a0ddb4a8ed6e58f68f6270cdc2b2822698c0df
KawiraSharon <sharonkawira389@gmail.com>
2023-11-16 18:21:07 +0300

    afterpRINTF

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,7 @@ size_t print_list(const list_t *h)
 {
 	/* initialize return data type, size_t*/
 	size_t node_no;
-	
+
 	node_no = 0;
 	/*loop around h list*/
 	while (h != NULL)
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[%d] %s", 0, "(nil)");
 		/*no node to return because the current node was calculated prev.*/
-		
+
 		else
 		{
 			printf("[%d] %s", h->len, h->str);
@@ -27,5 +27,5 @@ size_t print_list(const list_t *h)
 		node_no++;
 	}
 
-	return(node_no);
+	return (node_no);
 }
