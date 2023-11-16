8c916ca78237f3d7650938643b083371d94d012e
KawiraSharon <sharonkawira389@gmail.com>
2023-11-16 17:47:22 +0300

    AfterPrintf

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - function to print all elements in a singly linked list_t
+ * @h: arguments being passed to func, make the arg list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	/* initialize return data type, size_t*/
+	size_t node_no = 0;
+
+	if (h->str == NULL)
+		printf("[%d] %s", 0, "(nil)");
+	/*no node to return because the current node was calculated prev.*/
+	else
+	{
+		while (h != NULL)
+		{
+			printf("[%d] %s", h->len, h->str);
+			/*use h->next as the newly incremented value of h, not h++*/
+			h = h->next;
+			node_no++;
+		}
+	}
+	return(node_no);
+}
