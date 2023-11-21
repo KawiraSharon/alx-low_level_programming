aaae9371abfa821a716412a37c978381a8a4f781
KawiraSharon <sharonkawira389@gmail.com>
2023-11-22 01:26:34 +0300

    task

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,36 +1,37 @@
 #include "lists.h"
+
 /**
- * print_listint_safe - function that prints a linked list with a loop safely.
- * @head: pointer to the 1st node of the linked list
- * Return: new_node
+ * print_listint_safe - function to print linked list listint_t
+ * @head: pointer to first node in the list
+ * Return: number of nodes in list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-const listint_t *tmp_n = NULL;
-const listint_t *l_n = NULL;
-size_t counter = 0;
-size_t new_n;
+	const listint_t *ptrTemp = NULL;
+	const listint_t *listno = NULL;
+	size_t count = 0;
+	size_t ptrNew;
 
-tmp_n = head;
-while (tmp_n)
-{
-printf("[%p] %d\n", (void *)tmp_n, tmp_n->n);
-counter++;
-tmp_n = tmp_n->next;
-l_n = head;
-new_n = 0;
-while (new_n < counter)
-{
-if (tmp_n == l_n)
-{
-printf("-> [%p] %d\n", (void *)tmp_n, tmp_n->n);
-return (counter);
-}
-l_n = l_n->next;
-new_n++;
-}
-if (!head)
-exit(98);
-}
-return (counter);
+	ptrTemp = head;
+	while (ptrTemp)
+	{
+		printf("[%p] %d\n", (void *)ptrTemp, ptrTemp->n);
+		count++;
+		ptrTemp = ptrTemp->next;
+		listno = head;
+		ptrNew = 0;
+		while (ptrNew < count)
+		{
+			if (ptrTemp == listno)
+			{
+				printf("-> [%p] %d\n", (void *)ptrTemp, ptrTemp->n);
+				return (count);
+			}
+			listno = listno->next;
+			ptrNew++;
+		}
+		if (!head)
+			exit(98);
+	}
+	return (count);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -32,5 +32,6 @@ void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
 int sum_listint(listint_t *head);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+size_t print_listint_safe(const listint_t *head);
 
 #endif
