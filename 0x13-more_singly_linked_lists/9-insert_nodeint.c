aa5bf1ead8dc8c437ca008d5c42f922a9a1d92c8
KawiraSharon <sharonkawira389@gmail.com>
2023-11-21 17:12:07 +0300

    singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,6 +37,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	while (x < (idx - 1))
 	{
+		if (temp == NULL || temp->next == NULL)
+		{
+			free(ptrNewNode);
+			return (NULL);
+		}
 		temp = temp->next;
 		x++;
 	}
