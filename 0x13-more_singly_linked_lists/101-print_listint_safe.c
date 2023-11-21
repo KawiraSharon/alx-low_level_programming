5a187f4d8a5f0ea8d22d07a07f43da1e9a8dd73f
KawiraSharon <sharonkawira389@gmail.com>
2023-11-21 18:51:59 +0300

    singly linked lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,38 +1,36 @@
 #include "lists.h"
-
 /**
- * print_listint_safe - function that prints listint_t linked list
- * @head: pointer to first element in list
- * Return: number of nodes in list, or exit status 98 if fail
+ * print_listint_safe - function that prints a linked list with a loop safely.
+ * @head: pointer to the 1st node of the linked list
+ * Return: new_node
  */
 size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *ptrTemp = NULL;
 	const listint_t *ptrN = NULL;
-	size_t count_int = 0;
+	size_t count = 0;
 	size_t node_new;
 
 	ptrTemp = head;
 	while (ptrTemp)
 	{
-		printf("[%p] %d\n", (void *) ptrTemp, ptrTemp->n);
-		count_int++;
+		printf("[%p] %d\n", (void *)ptrTemp, ptrTemp->n);
+		count++;
 		ptrTemp = ptrTemp->next;
 		ptrN = head;
 		node_new = 0;
-		while (node_new < count_int)
+		while (node_new < count)
 		{
 			if (ptrTemp == ptrN)
 			{
-				printf("->[%p] %d\n", (void *)ptrTemp, ptrTemp->n);
-				return (count_int);
+				printf("-> [%p] %d\n", (void *)ptrTemp, ptrTemp->n);
+				return (count);
 			}
 			ptrN = ptrN->next;
 			node_new++;
 		}
+		if (!head)
+			exit(98);
 	}
-	if (head == NULL)
-		exit(98);
-
-	return (count_int);
+	return (count);
 }
