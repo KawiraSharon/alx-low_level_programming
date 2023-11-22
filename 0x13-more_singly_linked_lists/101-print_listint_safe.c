6d53d33dc78ecc2a78328c292d480b155f2d85d2
KawiraSharon <sharonkawira389@gmail.com>
2023-11-22 03:07:26 +0300

    thanks

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,7 +9,7 @@ size_t print_listint_safe(const listint_t *head)
 	const listint_t *ptrtemp = NULL;
 	const listint_t *list_no = NULL;
 	size_t count_node = 0;
-	size_t new_n;
+	size_t newnode;
 
 	ptrtemp = head;
 	while (ptrtemp)
@@ -18,8 +18,8 @@ size_t print_listint_safe(const listint_t *head)
 		count_node++;
 		ptrtemp = ptrtemp->next;
 		list_no = head;
-		new_n = 0;
-		while (new_n < count_node)
+		newnode = 0;
+		while (newnode < count_node)
 		{
 			if (ptrtemp == list_no)
 			{
@@ -27,7 +27,7 @@ size_t print_listint_safe(const listint_t *head)
 				return (count_node);
 			}
 			list_no = list_no->next;
-			new_n++;
+			newnode++;
 		}
 		if (!head)
 			exit(98);
