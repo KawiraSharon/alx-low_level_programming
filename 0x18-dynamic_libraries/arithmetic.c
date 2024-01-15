75e6ac82351fced45226c0918de56cf314c7b660
KawiraSharon <sharonkawira389@gmail.com>
2024-01-16 00:27:05 +0300

    more dynamic libraries created

---
+++ 0x18-dynamic_libraries/arithmetic.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - function sums two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: summation of the integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - function subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: subtraction of the integers
+ */
+int sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * mul - function multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of the integers
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * div - function divides two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: division of the integers
+ */
+int div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * mod - function gets modulus of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of the division of integers
+ */
+int mod(int a, int b)
+{
+        return (a % b);
+}
