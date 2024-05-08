3a20d077eb5a9a8e15347cfe09b378af83e7d767
= <=>
2024-05-09 00:49:09 +0300

    Task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,13 +1,11 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array of
- * integers using the Linear search algorithm
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: Always EXIT_SUCCESS
+ * linear_search - search array value at given idx
+ * @array: array input
+ * @size: size of arr
+ * @value: value to be searched for
+ * Return: EXIT_SUCCESS constantly
  */
 int linear_search(int *array, size_t size, int value)
 {
